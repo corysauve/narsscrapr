5850141a85ad37a88e24f8b3bec10b51f8b5113b
Cory Sauve <csauve95@gmail.com>
2021-10-10 21:03:28 -0400

    documenting data

---
+++ R/data.R
@@ -0,0 +1,15 @@
+#' NARS Data Links
+#'
+#' A dataset containing data links for NARS data
+#'
+#' format A data frame with 268 rows and 6 variables:
+#' \describe{
+#'  \item{survey_type}{NARS survey type}
+#'  \item{survey_year}{Survey year}
+#'  \item{indicator}{Indicator from survey}
+#'  \item{data}{Description of indicator data}
+#'  \item{data_url}{Link to data}
+#'  \item{metadata}{Link to metadata}
+#' }
+#'
+"nars_links"
